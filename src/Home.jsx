2eccbdef87e7988e8ab904dbff331fea6bae9d68
jojo11kerokero <jojo11kerokero@gmail.com>
2025-01-09 15:19:26 +0000

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,24 @@
+import './Home.css'
+
+
+function Home(){
+    
+    return(
+        <div className='HomeSection'>
+            {/* background */}
+            <div className='BgVideoWrapper'>
+                <video src="" className='BgVideo' />
+            </div>
+            {/* top */}
+            <div className='Header'>
+                <h1 className='Title'>
+                    <p>made by</p>
+                    <p>ETHER</p>
+                </h1>
+            </div>
+            <div> INSERT THE CONTENT HERE</div>
+        </div>
+    )
+}
+
+export default Home
