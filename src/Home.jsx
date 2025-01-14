23946ad47d87c840e6503b0723636f5da653328d
jojo11kerokero <jojo11kerokero@gmail.com>
2025-01-14 06:11:25 +0000

    added menu component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,22 +1,35 @@
 import './Home.css'
-
+import bg from './assets/Sims cat break-dancing.mp4'
 
 function Home(){
     
     return(
         <div className='HomeSection'>
             {/* background */}
-            <div className='BgVideoWrapper'>
-                <video src="" className='BgVideo' />
-            </div>
+            <video src='' className='BgVideo' loop autoPlay muted/>
             {/* top */}
-            <div className='Header'>
-                <h1 className='Title'>
-                    <p>made by</p>
-                    <p>ETHER</p>
-                </h1>
+            <div className='Header'>    
+                <div className='Title'>
+                    <h1>ETHER</h1>
+                    <p className='SubTitle'>made by the community.</p>
+                </div>
+                <div className='NavigationWrapper'> 
+                    <button className='NavigationButton'>Profile</button>
+                    <button className='NavigationButton'>Gallery</button>
+                    <button className='NavigationButton'>Roadmap</button>
+                    <button className='NavigationButton'>About</button>
+                </div>
             </div>
-            <div> INSERT THE CONTENT HERE</div>
+            {/* middle */}
+            <div className='Body'>
+                <div className='GridWrapper'>
+                        <div className="GridItem G1">Gallery</div>
+                        <div className="GridItem G1">INVENTORY</div>
+                        <div className="GridItem G2">RAIDS V2</div>
+                </div>
+            </div>
+            {/* bottom */}
+            <button className='EnterButton'> Enter The ETHER </button>
         </div>
     )
 }
---
+++ src/Menu.jsx
@@ -0,0 +1,21 @@
+import './Menu.css'
+
+
+function Menu (){
+    return(
+        <div className='MenuWrapper'>
+            <div className="Title">
+                MENU
+            </div>
+            <div className="GridWrapper">
+                <div className="GridItem FirstItem">item1</div>
+                <div className="GridItem">item2</div>
+                <div className="GridItem">item3</div>
+                <div className="GridItem">item4</div>
+                <div className="GridItem">item5</div>
+            </div>
+        </div>
+    )
+}
+
+export default Menu
